8abe947ff25cff798ec1d06ad297e3234cbc6db3
Frank <2274631388@qq.com>
2019-05-14 00:41:41 +0800

    ZZ

--- src/main/java/com/n33/jvm/learn/classloader/chapther3/MyObject.java
+++ src/main/java/com/n33/jvm/learn/classloader/chapther3/MyObject.java
@@ -1,5 +1,11 @@
 package com.n33.jvm.learn.classloader.chapther3;
 
+/**
+* 测试对象
+* 
+* @author N33
+* @date 2019/5/14
+*/
 public class MyObject {
     static {
         System.out.println("My object static block.");
